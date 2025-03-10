005571ecaf69e3664ff88efa8725f25adb20b0c6
Anoop George <anoopgeorge418@gmail.com>
2025-03-10 22:28:56 +0530

    Custom Exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,10 +1,10 @@
 import sys
-from logger import logging
+from src.loggerlogger import logging
 
 def error_message_details(error, error_detail:sys):
-    _, _, exc_tab = error_detail.exc_info()
-    file_name = exc_tab.tb_frame.f_code.co_filename
-    error_message = f"Error occured in python script name [{file_name}] line number [{exc_tab.tb_lineno}] error message [{str(error)}]"
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occured in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
     return error_message
 
 class  CustomException(Exception):
