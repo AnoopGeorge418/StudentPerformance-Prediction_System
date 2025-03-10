b157f0f900ed5507a306121547a4364e5b95c4f7
Anoop George <anoopgeorge418@gmail.com>
2025-03-10 13:56:27 +0530

    Converting project to packages

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages # type: ignore
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(path:str) -> list[str]:
+    '''This function will return the list of requirements'''
+    requirements = []
+    with open(path) as file_path:
+        requirements = (file_path.readlines())
+        requirements = ([requirement.replace('\n', '') for requirement in requirements])
+
+        # removing -e . from list cause it is used to trigger the setup.py
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name = 'StudentPerformance-Prediction_System',
+    version = '0.0.1',
+    description = 'A Model that allows users to predict the student performance absed on their scores.',
+    author = 'Anoop George',
+    author_email = 'anoopgeorge418@gmail.com',
+    maintainer = 'Anoop George',
+    maintainer_email = 'anoopgeorge418@gmail.com',
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = get_requirements('./requirements.txt')
+)
