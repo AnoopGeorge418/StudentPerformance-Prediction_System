98649f122d46393f20053dbc06235d6db083f800
Anoop George <anoopgeorge418@gmail.com>
2025-03-11 13:14:47 +0530

    Utility functions

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
+import os, sys
+import numpy as np # type: ignore
+import pandas as pd # type: ignore
+import dill # type: ignore
+from src.exceptions import CustomException
 
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok = True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
